457a12cca7462a2ff2a6b7ee712ec6968f466334
TheSouthernReach <985496515@qq.com>
2024-03-28 19:56:47 +0800

    本地

--- home/views.py
+++ home/views.py
@@ -11,13 +11,11 @@ from django.contrib import messages
 
 # Create your views here.
 
-
 def index(request):
 
-    # Page from the theme
+    # Page from the theme 
     return render(request, 'pages/index.html')
 
-# 主页面
 
 
 def home(request):
@@ -26,15 +24,11 @@ def home(request):
     }
     return render(request, 'pages/home.html', context)
 
-# 解决方案生成
-
-
 def smartAnalysis(request):
     context = {
         'segment': 'smart-analysis',
     }
     return render(request, 'pages/smart-analysis.html', context)
-
 
 def disputeCases(request):
     context = {
@@ -42,97 +36,62 @@ def disputeCases(request):
     }
     return render(request, 'pages/dispute-list.html', context)
 
-
 def lastNews(request):
     context = {
         'segment': 'last-news',
     }
     return render(request, 'pages/last-news.html', context)
 
-
-def myProvements(request):
-    context = {
-        'segment': 'my-provements',
-    }
-    return render(request, 'pages/my-provements.html', context)
-
-
-def lastNews(request):
-    context = {
-        'segment': 'last-news',
-    }
-    return render(request, 'pages/last-news.html', context)
-
-
-# 社区展示
-def successfulCases(request, page_id=1):
-
+def successfulCases(request):
     context = {
         'segment': 'successful-cases',
-        'page_id' : page_id, # 1: 最热 2: 最新 3: 点赞
-        'discussion': [
-            {
-                'cover': '/static/images/case-test.jpeg',
-                'title': '关于某某案件的讨论',
-                'content': 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-                'avatar': '/static/images/avatar/avt.jpg',
-                'username': '某某律师',
-                'date': '2021-07-01',
-            },
-            {
-                'cover': '/static/images/case-test.jpeg',
-                'title': '关于某某案件的讨论',
-                'content': 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-                'avatar': '/static/images/avatar/avt.jpg',
-                'username': '某某律师',
-                'date': '2021-07-01',
-            },
-            {
-                'cover': '/static/images/case-test.jpeg',
-                'title': '关于某某案件的讨论',
-                'content': 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-                'avatar': '/static/images/avatar/avt.jpg',
-                'username': '某某律师',
-                'date': '2021-07-01',
-            },
-            {
-                'cover': '/static/images/case-test.jpeg',
-                'title': '关于某某案件的讨论',
-                'content': 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-                'avatar': '/static/images/avatar/avt.jpg',
-                'username': '某某律师',
-                'date': '2021-07-01',
-            },
-            {
-                'cover': '/static/images/case-test.jpeg',
-                'title': '关于某某案件的讨论',
-                'content': 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-                'avatar': '/static/images/avatar/avt.jpg',
-                'username': '某某律师',
-                'date': '2021-07-01',
-            },
-            {
-                'cover': '/static/images/case-test.jpeg',
-                'title': '关于某某案件的讨论',
-                'content': 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-                'avatar': '/static/images/avatar/avt.jpg',
-                'username': '某某律师',
-                'date': '2021-07-01',
-            }
-        ],
-
     }
     return render(request, 'pages/successful-cases.html', context)
 
-# 社区发表文章
-
-
-def postCase(request):
+def myProvements(request):
     context = {
-        'segment': 'post',
+        'segment': 'my-provements',
     }
-    return render(request, 'pages/post.html', context)
+    return render(request, 'pages/my-provements.html', context)
 
+# Create your views here.
+
+def index(request):
+
+    # Page from the theme 
+    return render(request, 'pages/index.html')
+
+
+
+def home(request):
+    context = {
+        'segment': 'home',
+    }
+    return render(request, 'pages/home.html', context)
+
+def smartAnalysis(request):
+    context = {
+        'segment': 'smart-analysis',
+    }
+    return render(request, 'pages/smart-analysis.html', context)
+
+def disputeCases(request):
+    context = {
+        'segment': 'dispute-cases',
+    }
+    return render(request, 'pages/dispute-list.html', context)
+
+def lastNews(request):
+    context = {
+        'segment': 'last-news',
+    }
+    return render(request, 'pages/last-news.html', context)
+
+def successfulCases(request):
+    context = {
+        'segment': 'successful-cases',
+    }
+    return render(request, 'pages/successful-cases.html', context)
 
 def myProvements(request):
     context = {
@@ -140,8 +99,6 @@ def myProvements(request):
     }
     return render(request, 'pages/my-provements.html', context)
 
-
-# 证据上传
 @login_required
 def provements_upload(request):
     if request.method == 'POST':
@@ -150,41 +107,16 @@ def provements_upload(request):
         title = request.POST.get('title')
         content = request.POST.get('content')
         img_provements = request.FILES.getlist('img_provements')
-<<<<<<< Updated upstream
-
-        # 如果有图片上传，则处理图片
-=======
         file_provements = request.FILES.getlist('file_provements')
         file_info = request.POST.getlist('file_info')
         img_info = request.POST.getlist('img_info')
 
->>>>>>> Stashed changes
         img_ids = []
         if img_provements:
             for i, img in enumerate(img_provements):
                 # 获取对应的 img_info
                 current_img_info = img_info[i] if i < len(img_info) else ''
                 # 假设 Image 是你的图片模型
-<<<<<<< Updated upstream
-                image_id = Image.objects.create(image=img, intro=title).id
-                img_ids.append(image_id)
-
-        # 创建 Provements 对象，并将其与当前用户关联
-        pro = Provements.objects.create(
-            user=current_user, title=title, content=content)
-        if img_ids:
-            pro.img_provements.add(*img_ids)  # 将上传的图片关联到 Provements 对象中
-
-        # 保存 Provements 对象到数据库中
-        pro.save()
-
-        # 收集和处理附加信息
-        additional_info = request.POST.get('additional_info')
-        # TODO: Process additional_info as needed
-
-        # 重定向到某个页面或者返回一个成功的消息
-        return redirect('dispute-list')
-=======
                 image = Image.objects.create(image=img, intro=current_img_info)
                 img_ids.append(image.id)
 
@@ -213,7 +145,6 @@ def provements_upload(request):
         
         messages.success(request, '证据上传成功')
         return redirect('my-provements')
->>>>>>> Stashed changes
 
     return render(request, 'home.html')
 
