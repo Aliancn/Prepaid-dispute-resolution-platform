b322a7789728b31ee4d01cd0369cb70af82a50a4
chengningly <114815243+Aliancn@users.noreply.github.com>
2024-04-03 15:59:26 +0800

    部署测试3

--- core/settings.py
+++ core/settings.py
@@ -124,12 +124,15 @@ if DB_ENGINE and DB_NAME and DB_USERNAME:
         },
     }
 else:
+    # Replace the SQLite DATABASES configuration with PostgreSQL:
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': 'db.sqlite3',
-        }
+        'default': dj_database_url.config(
+            # Replace this value with your local database's connection string.
+            default='postgresql://postgres:postgres@localhost:5432/mysite',
+            conn_max_age=600
+        )
     }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
